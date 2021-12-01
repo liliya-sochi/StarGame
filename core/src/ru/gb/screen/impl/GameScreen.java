ce42f0f1506d3389f2bb652cb6c25246ae021d17
Liliya <liliya.sochi@mail.ru>
2021-12-01 21:40:42 +0300

    Домашнее Задание №4.

--- core/src/ru/gb/screen/impl/GameScreen.java
+++ core/src/ru/gb/screen/impl/GameScreen.java
@@ -1,20 +1,33 @@
 package ru.gb.screen.impl;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 import ru.gb.math.Rect;
 import ru.gb.screen.BaseScreen;
 import ru.gb.sprite.impl.Background;
+import ru.gb.sprite.impl.MainShip;
+import ru.gb.sprite.impl.Star;
 
 public class GameScreen extends BaseScreen {
+    private static final int STAR_COUNT = 64;
     private Texture bg;
     private Background background;
+    private TextureAtlas atlas;
+    private Star[] stars;
+    private MainShip mainShip;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+        mainShip = new MainShip(atlas);
     }
 
     @Override
@@ -28,6 +41,10 @@ public class GameScreen extends BaseScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        mainShip.resize(worldBounds);
     }
 
     @Override
@@ -38,21 +55,42 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        return super.touchDown(touch, pointer, button);
+        mainShip.touchDown(touch, pointer, button);
+        return false;
     }
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
-        return super.touchUp(touch, pointer, button);
+        mainShip.touchUp(touch, pointer, button);
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return false;
     }
 
     private void update(float delta) {
-
+        for (Star star : stars) {
+            star.update(delta);
+        }
+        mainShip.update(delta);
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        mainShip.draw(batch);
         batch.end();
     }
 }
